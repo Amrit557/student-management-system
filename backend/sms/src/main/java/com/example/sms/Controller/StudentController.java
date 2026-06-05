c4ce019dd6fe04416828b30b3396a1365d42e5c3
unknown <amritsahota710@example.com>
2026-06-05 14:22:15 +0530

    Day3 : ArrayList

--- backend/sms/src/main/java/com/example/sms/Controller/StudentController.java
+++ backend/sms/src/main/java/com/example/sms/Controller/StudentController.java
@@ -1,17 +1,28 @@
 package com.example.sms.Controller;
 
 import com.example.sms.model.Student;
+
+import java.util.ArrayList;
+
 import org.springframework.web.bind.annotation.*;
 
 
 
 @RestController //From Spring web
 @RequestMapping("/students") //http://localhost:8080/students
-@CrossOrigin(origins = "http://localhost:5173") //Allowing cross-origin requests from the React frontend
+@CrossOrigin(origins = "*") //Allowing cross-origin requests from the React frontend
 public class StudentController {
     
     @GetMapping
-    public Student getStudent() {
-        return new Student(1, "John Doe", "Computer Science");
+        public ArrayList<Student> getStudents() {
+
+        ArrayList<Student> students = new ArrayList<>();
+        students.add(new Student(1, "John Doe", "mba"));
+        students.add(new Student(2, "Jane Smith", "bba"));
+        students.add(new Student(3, "Alice Johnson", "bca"));
+
+        return students;
     }
+   
+    
 }
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,30 @@
 import { useState } from 'react';
 //? optional chaining app does not crash 
+
 function App(){
 
-    const [student, setStudent] = useState(null);
+    const [students, setStudents] = useState([]);
     //{"id":1,"name":"John Doe","course":"Computer Science"}
 
-    const getStudent = async () => {
+    const getStudents = async () => {
         const response = await fetch('http://localhost:8080/students');
         const data = await response.json();
-        setStudent(data);
+        setStudents(data);
     };
     return (
         <div>
 
-            <button onClick={getStudent}>
-                Fetch Student
+            <button onClick={getStudents}>
+                Get Students
             </button>
 
-            <h1>Here will be student data</h1>
-            <h1>{student?.id}</h1>
-            <h1>{student?.name}</h1> 
-            <h1>{student?.course}</h1>
+            <ul>
+                {students.map(student => (
+                    <li key={student.id}>
+                        {student.name} - {student.course}
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
